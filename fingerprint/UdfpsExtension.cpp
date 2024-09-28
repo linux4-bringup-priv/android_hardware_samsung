02dbe51831271848ba71c4e9715268020699c270
pjgowtham <pjgowtham@gmail.com>
2024-09-29 01:36:20 +0530

    fingerprint: Set dim layer zOrder
    Requires: I367b2c395aeb8cb7cb9e3cc38eca98477befe007
    Change-Id: I881db78186fa9c7b350e79ac4ef90f53ee4cfbcf

--- fingerprint/UdfpsExtension.cpp
+++ fingerprint/UdfpsExtension.cpp
@@ -1,6 +1,6 @@
 
 /*
- * Copyright (C) 2022 The LineageOS Project
+ * Copyright (C) 2022-2024 The LineageOS Project
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -12,6 +12,10 @@
 #elif __has_include(<drm/sde_drm.h>)
 #include <drm/sde_drm.h>
 #endif
+
+uint32_t getUdfpsDimZOrder(uint32_t z) {
+    return z;
+}
 
 uint32_t getUdfpsZOrder(uint32_t z, bool touched) {
     if (touched) {
